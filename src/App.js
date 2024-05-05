765c5df963cbd8468653c16fd6d3b4f1fc04cdb0
Isreality <isrealitycreations@gmail.com>
2024-05-05 09:55:07 +0100

    changed the modal

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import './App.css';
 import './style.css';
 import fisher from './img/fisher.jpg';
-// import cancel from './img/cancel.png';
 import cancel from './icons/cancel.svg';
+import success from './icons/success.svg';
 import { useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-// import { ToastContainer, toast } from 'react-toastify';
 
 
 function Modal({ message, type, onClose }) {
+  const modalClasses = `relative top-0 left-0 right-0 p-4 font-medium text-left z-50 ${type === 'success' ? 'bg-success2 text-success' : 'bg-red2 text-red'}`;
+  const iconSrc = type === 'success' ? success : cancel;
+  const iconColor = type === 'success' ? 'bg-success' : 'bg-red';
+
   return (
-    <div 
-      className='bg-red2 p-4 text-red text-left relative font-bold border-red rounded-md top-0 '
-      type= {'success' ? 'green' : 'red'}
-      // style={{
-      //   position: 'fixed',
-      //   top: 50,
-      //   left: 20,
-      //   right: 0,
-      //   // background: type === 'success' ? 'green' : 'red',
-      //   color: '#ffffff',
-      //   padding: '10px',
-      //   textAlign: 'center',
-      //   borderRadius: '20px',
-      // }}
-    >
+    <div className={modalClasses}>
       <div className='flex flex-row gap-10'>
-        <img src={cancel} alt="" className="bg-red p-4 absolute left-0 top-0 rounded-md"/>
-        
+        <img src={iconSrc} alt="" className={`p-4 absolute left-0 top-0 rounded-md ${iconColor}`}/>        
         <div className='ml-12'>{message}</div> 
       </div>
 
       <button style={{ marginLeft: '470px', top: '10px'}} onClick={onClose} className='absolute text-black2 top-50 left-20 font-normal'>
-          X
-      </button>
+          X</button>
     </div>
+          
   );
 }
 
@@ -51,24 +39,25 @@ function App() {
     setShowPassword(!showPassword);
   };
 
-  // const notify = () => toast("SIgn In Successfull!");
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    // Check if email and password are not empty
-     if (email.trim() === '') {
-      setErrorMessage('Email is required.');
-      setSuccessMessage('');
-      setIsModalOpen(true);
+    // Check if email and password are empty  
+    if (email.trim() === '' && password.trim() === ''){
+        setErrorMessage('Email and password are required.');
+        setSuccessMessage('');
+        setIsModalOpen(true);
       return;
-    }
-    if (password.trim() === '') {
-      setErrorMessage('Password is required.');
-      setSuccessMessage('');
-      setIsModalOpen(true);
-      return;
-    }
+      } else if (email.trim() === '') {
+        setErrorMessage('Email is required.');
+        setSuccessMessage('');
+        setIsModalOpen(true);
+        return;
+      } else if(password.trim() === '') {
+        setErrorMessage('Password is required.');
+        setSuccessMessage('');
+        setIsModalOpen(true);
+        return;
+      }
 
     // Check if email is in a valid format
     if (!/^\S+@\S+\.\S+$/.test(email)) {
@@ -78,19 +67,22 @@ function App() {
       return;
     }
 
-    if (email !== 'agbajeisrael01@gmail.com') {
-      setErrorMessage('You inputted the wrong email. Please try again!');
+    // Check if email and password are not given correct inputs
+    if (email !== 'agbajeisrael01@gmail.com' && password !== 'Oluwadante01') {
+      setErrorMessage('Your email and password are incorrect. Please try again!');
       setSuccessMessage('');
       setIsModalOpen(true);
       return;
-    }
-
-    // Check if password meets minimum length
-    if (password !== 'Oluwadante01') {
-      setErrorMessage('Incorrect Password');
-      setSuccessMessage('');
-      setIsModalOpen(true);
-      return;
+    } else if (email !== 'agbajeisrael01@gmail.com') {
+        setErrorMessage('You inputted the wrong email. Please try again!');
+        setSuccessMessage('');
+        setIsModalOpen(true);
+        return;
+    } else if (password !== 'Oluwadante01'){
+        setErrorMessage('Incorrect Password');
+        setSuccessMessage('');
+        setIsModalOpen(true);
+        return;
     }
 
     // Perform action if inputs are correct
@@ -113,11 +105,14 @@ function App() {
           {isModalOpen && (
               <Modal
                 message={errors || successMessage}
-                type={errors ? 'error' : 'success'}
+                type={errors ? 
+                  'error' : 
+                  'success'}
                 onClose={closeModal}
                 className="mb-24"
               />
             )}
+
           <h1 className='text-primary text-5xl font-bold mb-4'>Sign In</h1><br/>
           
           {/* Form */}
@@ -142,7 +137,7 @@ function App() {
             <div className='space-y-2' style={{ position: 'relative' }}>
               <label for="pwd" className='text-xl mb-8'>Password</label><br/>
               <input 
-                className='border-2 p-4 w-full rounded-md border-fa bg-fa focus:outline-primary' 
+                className='border-2 p-4 w-full rounded-md border-fa bg-fa focus:bg-fa focus:outline-primary' 
                 type= {showPassword ? 'text' : 'password'}
                 name = "pwd" 
                 placeholder='Enter your password'
@@ -189,13 +184,6 @@ function App() {
             value="Sign In" 
             className='mt-4 py-4 px-64 rounded-md border-fa bg-primary hover:bg-black cursor-pointer text-white text-xl font-bold'
             />
-            {/* {isModalOpen && (
-              <Modal
-                message={errors || successMessage}
-                type={errors ? 'error' : 'success'}
-                onClose={closeModal}
-              />
-            )} */}
           </form>
         </div>
       </div>
