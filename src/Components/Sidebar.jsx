afc2813ef5ccd83372b3a82c5049bdaadef6e94d
Isreality <isrealitycreations@gmail.com>
2024-05-08 01:43:33 +0100

    icons

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,25 +3,34 @@ import "../index.css";
 import SidebarData from "../Components/SidebarData";
 import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import logout from "../icons/sidebar/logout.svg"
 
 
 const Sidebar = () => {
     return ( 
         <>
-            <div className=" sticky h-screen left-0 w-60 text-left border-r-2 border-fa" >
-                <div className="p-2">
-                    <div className="flex flex-col justify-items-start mb-4 pl-6">                
-                    <Link to='/Dashboard'><h1 className="mb-8 mt-4 font-extrabold text-primary text-2xl">Market Access</h1></Link>
+            <div className=" sticky h-full left-0 w-64 text-left border-r-2 border-fa" >
+                <div className="p-4">
+                    <div className="flex flex-col justify-items-start mb-4 pl-2">                
+                    <Link to='/Dashboard'><h1 className="mb-4 mt-4 font-extrabold text-primary text-2xl">Market Access</h1></Link>
                     </div>
                     
                     {/* Nav */}
-                    <div className="flex justify-between lg:flex-col space-y-10">
+                    <div className="flex justify-between lg:flex-col space-y-2">
                         {SidebarData.map((nav) => (
-                            <div key={nav.id}>
-                                <NavLink to={nav.path} className=" m-2 text-black2 hover:text-black hover:bg-disable font-medium  hover:rounded-md hover:border-primary hover:min-w-full pl-4 pr-10 py-4">{nav.title}</NavLink>
-                            </div> 
+                            <NavLink to={nav.path} key={nav.id} className="flex flex-row gap-3 text-sm text-black2 hover:text-black2 hover:bg-disable font-medium  hover:rounded-md hover:border-primary pl-4 pr-10 py-4">
+                                <img src={nav.src} alt="icon" className='h-5 w-5'/>
+                                {/* <NavLink to={nav.path} className="nav">{nav.title}</NavLink> */}
+                                {nav.title}
+                            </NavLink> 
                         ))}
-                    </div> 
+                    </div><br/>
+
+                    {/* Logout */}   
+                    <div className="flex flex-row justify-items-center mb-4 pl-4 py-4 gap-3 text-sm text-black2 hover:text-black2 hover:bg-disable font-medium  hover:rounded-md hover:border-primary">
+                        <img src={logout} alt="" className='h-5 w-5'/>
+                        <button type="submit" >Logout</button>
+                    </div>
                 
                 </div>                    
             </div>
--- src/Components/SidebarData.jsx
+++ src/Components/SidebarData.jsx
@@ -1,58 +1,75 @@
 import "../style.css"
+import dash from "../icons/sidebar/dash.svg"
+import order from '../icons/sidebar/order.svg';
+import people from "../icons/sidebar/people.svg"
+import product from "../icons/sidebar/product.svg"
+import not from "../icons/sidebar/not.svg"
+import star from "../icons/sidebar/star.svg"
+import settings from "../icons/sidebar/settings.svg"
+
 
 const SidebarData =
     [
       {
         title: "Dashboard",
         path: "/Dashboard",
+        src: dash,
         id: 1
       },
   
       {
         title: "Order List",
         path: "/OrderList",
+        src: order,
         id: 2
       },
 
       {
         title: "Staff Admins",
         path: "/StaffAdmins",
+        src: people,
         id: 3
       },
 
       {
         title: "Vendors",
         path: "/Vendors",
+        src: people,
         id: 4
       },
 
       {
         title: "Customers",
         path: "/Customers",
+        src: people,
         id: 5
       },
 
       {
         title: "Manage Products",
         path: "/ManageProducts",
+        src: product,
         id: 6
       },
 
       {
         title: "Push Notification",
         path: "/PushNotification",
+        src: not,
         id: 7
       },
 
       {
         title: "Reviews",
         path: "/Reviews",
+        src: star,
         id: 8
       },
 
       {
         title: "Settings",
         path: "/Settings",
+        src: settings,
         id: 9
       },
     ]
