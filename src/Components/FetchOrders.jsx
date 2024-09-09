491f1eb3c30a49708e08724272f44b83a72dde16
Isreality <isrealitycreations@gmail.com>
2024-09-09 20:06:22 +0100

    searvh

--- src/Components/FetchOrders.jsx
+++ src/Components/FetchOrders.jsx
@@ -15,177 +15,194 @@ const FetchOrders = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [isDetailsModalOpen, setIsDetailsModalOpen] = useState(false);
     const [details, setDetails] = useState({});
-
+    const [searchTerm, setSearchTerm] = useState(""); // State for search term
     const [currentPage, setCurrentPage] = useState(1); 
     const itemsPerPage = 5;
 
-  // Function to assign color based on status
-  const getStatusColorClass = (status) => {
-    switch (status) {
-        case 'CANCELLED':
-            return 'bg-black'; // Red for cancelled
-        case 'ORDER_PLACED':
-            return 'bg-pend'; // Blue for order placed
-        case 'PENDING_CONFIRMATION':
-            return 'bg-orange-500'; // Yellow for pending confirmation
-        case 'WAITING_TO_BE_SHIPPED':
-            return 'bg-primary'; // Orange for waiting to be shipped
-        case 'OUT_FOR_DELIVERY':
-            return 'bg-teal-500'; // Teal for out for delivery
-        case 'SHIPPED':
-            return 'bg-success'; // Green for shipped
-        default:
-            return 'bg-gray-500'; // Default color for any unknown status
-    }
-  };
-
-  // Function to format boolean to "Yes" or "No" with color
-  const formatBooleanToYesNoWithColor = (value) => {
-    return (
-        <span className={value ? 'text-succes' : 'text-red'}>
-            {value ? 'Yes' : 'No'}
-        </span>
-    );
-};
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-    const startIdx = (page - 1) * itemsPerPage;
-    const endIdx = startIdx + itemsPerPage;
-    setDisplayData(data.slice(startIdx, endIdx));
-  };
-
-  const handleView = (item) => {
-    setDetails(item);
-    setIsDetailsModalOpen(true);
-  };
-
-  const closeDetailsModal = (item) => {
-    setIsDetailsModalOpen(false);
-  };
-
-  const baseURL = process.env.REACT_APP_BASE_URL;
-  const endpoint = '/admin/order/fetch-order-items?page=1';
-  const Atoken = JSON.parse(sessionStorage.getItem('data')).token.original.access_token;
-
-
-  // useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(baseURL + endpoint, {
-          method: 'GET',
-          headers: {
-            'Authorization': `Bearer ${Atoken}`,
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'ngrok-skip-browser-warning': "69420",
-            'origin': '*',
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const result = await response.json();
-        if (result.status) {
-          setData(result.data);
-          setDisplayData(result.data.slice(0, itemsPerPage));
-        } else {
-          throw new Error('Data fetch unsuccessful');
-        }
-      } catch (error) {
-        setError(error.message);
+    // Function to assign color based on status
+    const getStatusColorClass = (status) => {
+      switch (status) {
+          case 'CANCELLED':
+              return 'bg-black'; // Black for cancelled
+          case 'ORDER_PLACED':
+              return 'bg-pend'; // Yellow for order placed
+          case 'PENDING_CONFIRMATION':
+              return 'bg-orange-500'; // Orange for pending confirmation
+          case 'WAITING_TO_BE_SHIPPED':
+              return 'bg-primary'; // Purple for waiting to be shipped
+          case 'OUT_FOR_DELIVERY':
+              return 'bg-teal-500'; // Teal for out for delivery
+          case 'SHIPPED':
+              return 'bg-success'; // Green for shipped
+          default:
+              return 'bg-gray-500'; // Default color for any unknown status
       }
     };
 
+    // Function to format boolean to "Yes" or "No" with color
+    const formatBooleanToYesNoWithColor = (value) => {
+        return (
+            <span className={value ? 'text-success' : 'text-red'}>
+                {value ? 'Yes' : 'No'}
+            </span>
+        );
+    };
 
-  useEffect(() => {
-    fetchData();
-  }, [Atoken]);
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
 
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-        setLoading(false)
-    }, 3000)
-  }, [])
+    const handleView = (item) => {
+        setDetails(item);
+        setIsDetailsModalOpen(true);
+    };
 
-  if (loading) {
+    const closeDetailsModal = () => {
+        setIsDetailsModalOpen(false);
+    };
+
+    const handleSearchChange = (e) => {
+        const term = e.target.value;
+        setSearchTerm(term);
+    };
+
+    const baseURL = process.env.REACT_APP_BASE_URL;
+    const endpoint = '/admin/order/fetch-order-items?page=1';
+    const Atoken = JSON.parse(sessionStorage.getItem('data')).token.original.access_token;
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(baseURL + endpoint, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${Atoken}`,
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'ngrok-skip-browser-warning': "69420",
+                    'origin': '*',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const result = await response.json();
+            if (result.status) {
+                setData(result.data);
+                setDisplayData(result.data); // Initially show all data
+            } else {
+                throw new Error('Data fetch unsuccessful');
+            }
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [Atoken]);
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    }, []);
+
+    useEffect(() => {
+        // Filter data whenever searchTerm or data changes
+        const filteredData = data.filter(item =>
+            item.orderItemsRef.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        const startIdx = (currentPage - 1) * itemsPerPage;
+        const endIdx = startIdx + itemsPerPage;
+        setDisplayData(filteredData.slice(startIdx, endIdx));
+    }, [searchTerm, currentPage, data]);
+
+    if (loading) {
+        return (
+            <div>
+                <ScaleLoader
+                    color={'#c4c4c4'}
+                    loading={loading}
+                    size={50}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                /> 
+            </div>
+        );
+    }
+
+    if (data.length === 0) {
+        return (
+            <div className="flex flex-col items-center justify-center h-64">
+                <RiFileList3Line className="text-9xl text-c4"/>
+                <p className="text-lg text-black2">No order has been added</p>
+            </div>
+        );
+    }
+
     return (
-      <div>
-        <ScaleLoader
-          color={'#c4c4c4'}
-          loading={loading}
-          size={50}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        /> 
-      </div>
-    );
-  }
+        <div>
+            {/* Search Input */}
+            <div className="text-left mb-4">
+                <input
+                    type="text"
+                    placeholder="Search by Order Ref."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    className="p-2 border border-f2 rounded-sm focus:bg-white focus:outline-primary"
+                />
+            </div>
 
-  if (data.length === 0) {
-    return (
-      <div className="flex flex-col items-center justify-center h-64">
-        <RiFileList3Line className="text-9xl text-c4"/>
-        <p className="text-lg text-black2">No order has been added</p>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <div className="">
-
+            <div>
                 <table className="min-w-full border-collapse border border-disable py-4">
                     <thead className="bg-fa text-sm text-left">
                         <tr className="px-4 py-8">
                             <th className="px-4 py-6 text-black2 font-normal">Order Reference</th>
                             <th className="px-4 py-6 text-black2 font-normal">Total Amount</th>
-                            {/* <th className="px-4 py-6 text-black2 font-normal">Payment Ref.</th> */}
                             <th className="px-4 py-6 text-black2 font-normal">Status</th>
                             <th className="px-4 py-6 text-black2 font-normal">Payback</th>
                             <th className="px-4 py-6 text-black2 font-normal">Date</th>
                             <th className="px-4 py-6 text-black2 font-normal">Action</th>
                         </tr>
                     </thead>
-
                     <tbody className="text-left">
-                        {data.map((item) => (
+                        {displayData.map((item) => (
                             <tr key={item.id} className="text-black2 text-sm border-b border-disable">
                                 <td className="px-4 py-6">{item.orderItemsRef}</td>
                                 <td className="px-4 py-6">{item.product.price}</td>
-                                {/* <td className="px-4 py-6"></td> */}
-                                <td className='flex flex-row gap-1 px-4 py-6 items-center'>
-                                    <span className={`inlline-block h-2 w-2 rounded-full ${getStatusColorClass(item.orderState)}`}></span>
+                                <td className="px-4 py-6 flex items-center">
+                                    <span 
+                                        className={`inline-block w-2 h-2 rounded-full mr-2 ${getStatusColorClass(item.orderState)}`}
+                                    ></span>
                                     {item.orderState}
                                 </td>
                                 <td className="px-4 py-6">{formatBooleanToYesNoWithColor(item.isPayBackLater)}</td>
                                 <td className="px-4 py-6">{item.createdDate}</td>
                                 <td className="flex flex-row gap-2 px-4 py-6 items-center">
                                     <FaEye className="text-c4 size-5 cursor-pointer" onClick={() => handleView(item)}/>
-                                    {/* <HiOutlineTrash className="text-red size-5 cursor-pointer" onClick={() => openModal(item.id)}/> */}
                                 </td>
                             </tr>
                         ))}
                     </tbody>
-            </table>        
-                    {/* Pagination Component */}
-                    {data.length > itemsPerPage && (
-                        <ResponsivePagination
-                          total={Math.ceil(data.length / itemsPerPage)}
-                          current={currentPage}
-                          onPageChange={handlePageChange}
-                        />
-                    )}<br/>
-
-                  <ViewOrders
+                </table>
+                {data.length > itemsPerPage && (
+                    <ResponsivePagination
+                        total={Math.ceil(data.filter(item => 
+                            item.orderItemsRef.toLowerCase().includes(searchTerm.toLowerCase())
+                        ).length / itemsPerPage)}
+                        current={currentPage}
+                        onPageChange={handlePageChange}
+                    />
+                )}<br/>
+                <ViewOrders
                     show={isDetailsModalOpen}
                     handleClose={closeDetailsModal}
-                  />
-      </div>
-    </div>
-  );
+                />
+            </div>
+        </div>
+    );
 }
 
 export default FetchOrders;
