f1c915bae64bfbafd256df9b520f10e179dd49d7
Isreality <isrealitycreations@gmail.com>
2024-09-10 10:02:09 +0100

    filter

--- src/Components/FetchOrders.jsx
+++ src/Components/FetchOrders.jsx
@@ -3,6 +3,7 @@ import "../pagination.css";
 import { useState, useEffect } from 'react';
 import { FaEye } from 'react-icons/fa';
 import { RiFileList3Line } from "react-icons/ri";
+import { RiArrowDropDownLine } from "react-icons/ri";
 import ViewOrders from '../Components/ViewOrders';
 import ScaleLoader from "react-spinners/ScaleLoader";
 import ResponsivePagination from 'react-responsive-pagination';
@@ -15,7 +16,9 @@ const FetchOrders = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [isDetailsModalOpen, setIsDetailsModalOpen] = useState(false);
     const [details, setDetails] = useState({});
-    const [searchTerm, setSearchTerm] = useState(""); // State for search term
+    const [searchTerm, setSearchTerm] = useState(""); 
+    const [selectedStatus, setSelectedStatus] = useState("");
+    const [payBack, setPayBack] = useState("");
     const [currentPage, setCurrentPage] = useState(1); 
     const itemsPerPage = 5;
 
@@ -66,6 +69,14 @@ const FetchOrders = () => {
         setSearchTerm(term);
     };
 
+    const handleStatusChange = (e) => {
+        setSelectedStatus(e.target.value);
+    };
+
+    const handlePayBackChange = (e) => {
+        setPayBack(e.target.value);
+    };
+
     const baseURL = process.env.REACT_APP_BASE_URL;
     const endpoint = '/admin/order/fetch-order-items?page=1';
     const Atoken = JSON.parse(sessionStorage.getItem('data')).token.original.access_token;
@@ -110,14 +121,18 @@ const FetchOrders = () => {
     }, []);
 
     useEffect(() => {
-        // Filter data whenever searchTerm or data changes
+        // Filter data based on both search term and selected status
         const filteredData = data.filter(item =>
-            item.orderItemsRef.toLowerCase().includes(searchTerm.toLowerCase())
+            item.orderItemsRef.toLowerCase().includes(searchTerm.toLowerCase()) &&
+            (selectedStatus === "" || item.orderState === selectedStatus) &&
+            (payBack === "" || item.isPayBackLater === payBack)
         );
+
         const startIdx = (currentPage - 1) * itemsPerPage;
         const endIdx = startIdx + itemsPerPage;
         setDisplayData(filteredData.slice(startIdx, endIdx));
-    }, [searchTerm, currentPage, data]);
+    }, [searchTerm, selectedStatus, payBack, currentPage, data]);
+
 
     if (loading) {
         return (
@@ -145,16 +160,57 @@ const FetchOrders = () => {
     return (
         <div>
             {/* Search Input */}
-            <div className="text-left mb-4">
+            <div className=" flex flex-row justify-between text-left mb-4">
                 <input
                     type="text"
                     placeholder="Search by Order Ref."
                     value={searchTerm}
                     onChange={handleSearchChange}
-                    className="p-2 border border-f2 rounded-sm focus:bg-white focus:outline-primary"
+                    className="p-4 border border-f2 rounded focus:bg-white focus:outline-primary"
                 />
+
+                <div className="flex flex-row gap-2 items-center">
+                    {/* Filter by Status */}
+                    <div className='relative'>
+                        <select
+                            value={selectedStatus}
+                            onChange={handleStatusChange}
+                            className="block appearance-none py-4 px-8 bg-fa rounded focus:outline-primary cursor-pointer"
+                        >
+                            <option className="text-md" value="">All Statuses</option>
+                            <option className="text-md" value="CANCELLED">Cancelled</option>
+                            <option className="text-md" value="ORDER_PLACED">Order Placed</option>
+                            <option className="text-md" value="PENDING_CONFIRMATION">Pending Confirmation</option>
+                            <option className="text-md" value="WAITING_TO_BE_SHIPPED">Waiting to be shipped</option>
+                            <option className="text-md" value="OUT_FOR_DELIVERY">Out for Delivery</option>
+                            <option className="text-md" value="SHIPPED">Shipped</option>
+                        </select>
+
+                        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-black2">
+                            <RiArrowDropDownLine className="h-6 w-6"/>
+                        </div>
+                    </div>
+
+                    {/* Filter by Payback */}
+                    <div className='relative'>
+                        <select
+                            value={payBack}
+                            onChange={handlePayBackChange}
+                            className="block appearance-none py-4 px-8 bg-fa rounded focus:outline-primary cursor-pointer"
+                        >
+                            <option className="text-md" value="">Payback</option>
+                            <option className="text-md" value="Yes">Yes</option>
+                            <option className="text-md" value="No">No</option>
+                        </select>
+
+                        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-black2">
+                            <RiArrowDropDownLine className="h-6 w-6"/>
+                        </div>
+                    </div>
+                </div>
             </div>
 
+            {/* Table */}
             <div>
                 <table className="min-w-full border-collapse border border-disable py-4">
                     <thead className="bg-fa text-sm text-left">
@@ -167,6 +223,8 @@ const FetchOrders = () => {
                             <th className="px-4 py-6 text-black2 font-normal">Action</th>
                         </tr>
                     </thead>
+
+                    {/* Table body */}
                     <tbody className="text-left">
                         {displayData.map((item) => (
                             <tr key={item.id} className="text-black2 text-sm border-b border-disable">
@@ -187,15 +245,21 @@ const FetchOrders = () => {
                         ))}
                     </tbody>
                 </table>
+
+                {/* Pagination */}
                 {data.length > itemsPerPage && (
                     <ResponsivePagination
                         total={Math.ceil(data.filter(item => 
-                            item.orderItemsRef.toLowerCase().includes(searchTerm.toLowerCase())
+                            item.orderItemsRef.toLowerCase().includes(searchTerm.toLowerCase()) &&
+                            (selectedStatus === "" || item.orderState === selectedStatus) &&
+                            (payBack === "" || item.isPayBackLater === payBack)
                         ).length / itemsPerPage)}
                         current={currentPage}
                         onPageChange={handlePageChange}
                     />
                 )}<br/>
+
+                {/* View Order */}
                 <ViewOrders
                     show={isDetailsModalOpen}
                     handleClose={closeDetailsModal}
